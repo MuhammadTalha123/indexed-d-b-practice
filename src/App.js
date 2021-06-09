a1b3d97b8980ebce5f33c300f8e3976dc1a2d6fc
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-06-10 01:11:55 +0500

    retrieve from idb

--- src/App.js
+++ src/App.js
@@ -5,14 +5,28 @@ import { v4 as uuid } from "uuid";
 
 function App() {
   const [value, setValue] = useState("");
+  const [retriveValue, setRetriveValue] = useState([]);
   const handleInputValue = (e) => {
     setValue(e.target.value);
   };
 
   const handleSaveValue = async () => {
+    let uid = uuid();
     const db = await initializeDB("ToDoData");
-    await db.put("data", value, uuid());
+    await db.put("data", { id: uid, value }, uid);
     db.close();
+  };
+
+  const handleRetriveValue = async () => {
+    const db = await initializeDB("ToDoData");
+    const retriveDat = await db.getAll("data");
+    db.close();
+    console.log(retriveDat);
+    setRetriveValue(retriveDat);
+  };
+
+  const handleDeleteValue = async () => {
+    const db = await initializeDB("ToDoData");
   };
 
   return (
@@ -21,15 +35,19 @@ function App() {
       <div>
         <input type="text" value={value} onChange={handleInputValue} />
         <button onClick={handleSaveValue}>Save in DB</button>
-        <button>Retrive from DB</button>
+        <button onClick={handleRetriveValue}>Retrive from DB</button>
       </div>
-      <div>
-        <ul>
-          <li>item</li>
-        </ul>
-        <button>Update</button>
-        <button>Delete</button>
-      </div>
+      {retriveValue.map((item) => {
+        return (
+          <div key={item.id}>
+            <ul>
+              <li>{item.value}</li>
+            </ul>
+            <button>Update</button>
+            <button onClick={handleDeleteValue}>Delete</button>
+          </div>
+        );
+      })}
     </div>
   );
 }
