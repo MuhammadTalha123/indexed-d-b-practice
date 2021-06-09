77428da61f68448fe404273464a12805e5307507
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-06-10 01:43:38 +0500

    update ad delete data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { initializeDB } from "./IDB";
 import { v4 as uuid } from "uuid";
@@ -6,15 +6,29 @@ import { v4 as uuid } from "uuid";
 function App() {
   const [value, setValue] = useState("");
   const [retriveValue, setRetriveValue] = useState([]);
+  const [update, setUpdate] = useState(false);
+  const [updateKey, setUpdateKey] = useState(null);
+  useEffect(() => {
+    handleRetriveValue();
+  }, []);
   const handleInputValue = (e) => {
     setValue(e.target.value);
   };
 
   const handleSaveValue = async () => {
-    let uid = uuid();
-    const db = await initializeDB("ToDoData");
-    await db.put("data", { id: uid, value }, uid);
-    db.close();
+    if (update) {
+      const db = await initializeDB("ToDoData");
+      await db.put("data", { value, updateKey }, updateKey);
+      db.close();
+      setUpdate(false);
+    } else {
+      let uid = uuid();
+      const db = await initializeDB("ToDoData");
+      await db.put("data", { id: uid, value }, uid);
+      db.close();
+    }
+    setValue("");
+    window.location.reload();
   };
 
   const handleRetriveValue = async () => {
@@ -25,8 +39,17 @@ function App() {
     setRetriveValue(retriveDat);
   };
 
-  const handleDeleteValue = async () => {
+  const handleDeleteValue = async (key) => {
     const db = await initializeDB("ToDoData");
+    await db.delete("data", key);
+    db.close();
+    window.location.reload();
+  };
+
+  const handleUpdateValue = async ({ id, value }) => {
+    setValue(value);
+    setUpdate(true);
+    setUpdateKey(id);
   };
 
   return (
@@ -35,7 +58,7 @@ function App() {
       <div>
         <input type="text" value={value} onChange={handleInputValue} />
         <button onClick={handleSaveValue}>Save in DB</button>
-        <button onClick={handleRetriveValue}>Retrive from DB</button>
+        {/*<button onClick={handleRetriveValue}>Retrive from DB</button>*/}
       </div>
       {retriveValue.map((item) => {
         return (
@@ -43,8 +66,20 @@ function App() {
             <ul>
               <li>{item.value}</li>
             </ul>
-            <button>Update</button>
-            <button onClick={handleDeleteValue}>Delete</button>
+            <button
+              onClick={() => {
+                handleUpdateValue(item);
+              }}
+            >
+              Update
+            </button>
+            <button
+              onClick={() => {
+                handleDeleteValue(item.id);
+              }}
+            >
+              Delete
+            </button>
           </div>
         );
       })}
