fe4a0630b7203370b61bfe75c509b00433a74bb4
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-06-10 00:29:10 +0500

    open idb

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+  const handleInputValue = (e) => {
+    setValue(e.target.value);
+  };
+
+
+
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <h1>Indexed DB Practice</h1>
+      <div>
+        <input type="text" value={value} onChange={handleInputValue} />
+        <button>Save in DB</button>
+        <button>Retrive from DB</button>
+      </div>
+      <div>
+        <ul>
+          <li>item</li>
+        </ul>
+        <button>Update</button>
+        <button>Delete</button>
+      </div>
     </div>
   );
 }
